83f02252c68de22e6aa811a2f466bcc852dd6c2d
Nikules <48965760+Nikules@users.noreply.github.com>
2019-04-20 15:11:23 +0300

    Update Starting.erl
    Added: 
    len
    is_palindrome

--- Starting.erl
+++ Starting.erl
@@ -7,6 +7,9 @@
 -export([last2/1]).
 -export([element_at/2]).
 -export([reverse/1]).
+-export([is_palindrome/1]).
+-export([len/1]).
+
 
 add(A, B) ->
 A + B.
@@ -16,24 +19,38 @@ reside(A, B) ->
 A - B.
 
 
-last1([]) -> [];
+last1([]) 	-> {no_elements_in_list};
 last1([Answer]) -> Answer;
-last1([H|T]) -> last1(T).
+last1([H|T]) 	-> last1(T).
 
 
-last2([]) -> [];
-last2([Answer]) -> Answer;
-last2([Answer1, Answer2]) -> {Answer1, Answer2};
-last2([H|T]) -> last2(T).
+last2([]) 			-> {no_elements_in_list};
+last2([Answer]) 		-> Answer;
+last2([Answer1, Answer2]) 	-> {Answer1, Answer2};
+last2([H|T]) 			-> last2(T).
 
 
-element_at([], N) -> [];
-element_at([H|T], N) when N >= 0 ->
-if N == 1 -> H;
-	N > 1 -> element_at(T, N - 1)
+element_at([], N) 			-> [];
+element_at([H|T], N) when N >= 0 	->
+if 	N == 1 	-> H;
+	N > 1 	-> element_at(T, N - 1)
 end.
 
 
-reverse(Numbers) -> reverse(Numbers, []).
-reverse([], Acc) -> Acc;
-reverse([H|T], Acc) -> reverse(T, [H|Acc]).
+len([]) 		-> 0;
+len([_]) 		-> 1;
+len(List) 		-> len(List, 0).
+len([], N) 		-> N;
+len([H|T], N) 	-> len(T, N + 1).
+
+
+reverse(Numbers) 	-> reverse(Numbers, []).
+reverse([], Acc) 	-> Acc;
+reverse([H|T], Acc) 	-> reverse(T, [H|Acc]).
+
+
+is_palindrome(L) 	-> is_palindrome(L, reverse(L)).
+is_palindrome(L, M) 	->
+if L == M -> {true};
+	L =/= M -> {false}
+end.
