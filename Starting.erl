538bb68b79cca63f4643f668cbc76454a76943bb
Nikita Verhovodov <48965760+Nikules@users.noreply.github.com>
2019-05-10 11:56:39 +0300

    Update Starting.erl
    last1, last2, element_at fixed

--- Starting.erl
+++ Starting.erl
@@ -1,8 +1,7 @@
 -module(project).
 
 
--export([add/2]).
--export([reside/2]).
+
 -export([last1/1]).
 -export([last2/1]).
 -export([element_at/2]).
@@ -13,30 +12,19 @@
 
 
 
-add(A, B) ->
-A + B.
-
-
-reside(A, B) ->
-A - B.
-
-
-last1([]) 	-> {no_elements_in_list};
 last1([Answer]) -> Answer;
 last1([H|T]) 	-> last1(T).
 
 
-last2([]) 			-> {no_elements_in_list};
-last2([Answer]) 		-> Answer;
+
 last2([Answer1, Answer2]) 	-> {Answer1, Answer2};
 last2([H|T]) 			-> last2(T).
 
 
-element_at([], N) 			-> [];
-element_at([H|T], N) when N >= 0 	->
-	if 	N == 1 	-> H;
-		N > 1 	-> element_at(T, N - 1)
-	end.
+
+element_at([H|T], 1) 	-> H;
+element_at([H|T], N) 	-> element_at(T, N - 1).
+
 
 
 len([]) 		-> 0;
@@ -46,9 +34,11 @@ len([], N) 		-> N;
 len([H|T], N) 		-> len(T, N + 1).
 
 
+
 reverse(Numbers) 	-> reverse(Numbers, []).
 reverse([], Acc) 	-> Acc;
 reverse([H|T], Acc) 	-> reverse(T, [H|Acc]).
+
 
 
 is_palindrome(L)	->
